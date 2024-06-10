b65d7c950790f8019bf695671447e1ccbb680b89
Nicholas <lesetjamaluleka@gmail.com>
2024-06-10 12:43:08 +0200

    first commit

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "https://111e-169-239-176-9.ngrok-free.app/api/",
+        "tenantName": "neutrinos-delivery",
+        "appName": "fnb-external",
+        "namespace": "com.neutrinos-delivery.fnb-external",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
